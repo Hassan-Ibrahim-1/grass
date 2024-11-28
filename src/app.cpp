dd13ef9a8920fb1fdc96f9c48759509e2723fa08
Hassan-Ibrahim-1 <hassanibrahimasi@gmail.com>
2024-11-28 10:43:27 -0500

    grass that moves - kinda

--- src/app.cpp
+++ src/app.cpp
@@ -42,16 +42,37 @@ void App::init() {
             glm::vec3(-0.5f, 0.5f, 0.0f),
             glm::vec3(0, 0, 1),
             glm::vec2(0, 1)
+        },
+        {
+            glm::vec3(0, 1, 0),
+            glm::vec3(0, 0, 1),
+            glm::vec2(0, 1)
         }
     };
     mesh.indices = {
         0, 1, 3,
-        1, 2, 3 
+        1, 2, 3,
+        0, 3, 4
     };
+    mesh.draw_command.mode = DrawCommandMode::TRIANGLES;
+    mesh.draw_command.type = DrawCommandType::DRAW_ELEMENTS;
     mesh.create_buffers();
+    grass.transform.scale.x = 0.1;
+    grass.material.color = Color(0, 255, 141);
 }
 
 void App::update() {
+    float offset = 0.001 * cos(glfwGetTime());
+    auto& mesh = grass.meshes.front();
+    mesh.vertices[4].position.x += offset;
+    mesh.vertices[3].position.x += offset;
+    mesh.vertices[0].position.x += offset;
+    /*mesh.vertices[4].position.z += offset;*/
+    /*mesh.vertices[3].position.z += offset;*/
+    /*mesh.vertices[0].position.z += offset;*/
+    mesh.delete_buffers();
+    mesh.create_buffers();
+
     if (engine::cursor_enabled) {
         ImGui::Begin("scene");
         utils::imgui_game_object("grass", grass);
